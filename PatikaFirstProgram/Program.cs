fd49d7f337cb78ed9002181c9c310c76ce985f84
Enes KILIÇ <engineereneskilic@gmail.com>
2024-09-30 22:24:38 +0300

    Add project files.

---
+++ PatikaFirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PatikaFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENES KILIÇ");
+
+            Console.WriteLine("C# Backend geliştirme alanında derinlemesine bilgi edinmek, yazılım dünyasında güçlü adımlar atmak ve kariyerimde önemli bir sıçrama yapmak için Patika Bootcamp'ine katıldım.Bu süreçte, sektörde aranan bir yazılım geliştirici olma yolunda hem teknik hem de profesyonel yetkinliklerimi artırmayı hedefliyorum.");
+        }
+    }
+}
